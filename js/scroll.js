c51d3c67349c91d89eaa66f9a8380fabdc3ed65e
villamzr <villamzar@gmail.com>
2018-08-23 22:02:29 -0500

    agregado scroll.js para saltos de contenido con jquery

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,19 @@
-$('#profile')
+$('#profile').on('click', function(){
+	$('html, body').animate({scrollTop:0}, 1000);
+})
+
+$('#sobremi').on('click', function(){
+	$('html, body').animate({scrollTop:500}, 1000);
+})
+
+$('#educacion').on('click', function(){
+	$('html, body').animate({scrollTop:1000}, 1000);
+})
+
+$('#experiencia').on('click', function(){
+	$('html, body').animate({scrollTop:600}, 1000);
+})
+
+$('#portafolio').on('click', function(){
+	$('html, body').animate({scrollTop:700}, 1000);
+})
