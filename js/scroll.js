a209ffb520e224ba12bc154a18fcb292a3c9e27b
villamzr <villamzar@gmail.com>
2018-08-28 19:38:33 -0500

    Cambié EXPERIENCIA por HABILIDADES, y calibré el scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,29 @@
 
 if (screen.width <=700) {
 
-	$('#prof').on('click', function(){
+	/*$('#prof').on('click', function(){
 		$('html, body').animate({scrollTop:0}, 1000);
-	})
+	});
 
 	$('#profile').on('click', function(){
 		$('html, body').animate({scrollTop:0}, 1000);
-	})
+	});
 
 	$('#sobremi').on('click', function(){
 		$('html, body').animate({scrollTop:0}, 1000);
-	})
+	});
 
 	$('#educacion').on('click', function(){
 		$('html, body').animate({scrollTop:800}, 1000);
-	})
+	});
 
 	$('#experiencia').on('click', function(){
 		$('html, body').animate({scrollTop:600}, 1000);
-	})
+	});
 
 	$('#portafolio').on('click', function(){
 		$('html, body').animate({scrollTop:700}, 1000);
-	})
+	});*/
 
 }else{
 
@@ -40,15 +40,15 @@ if (screen.width <=700) {
 	})
 
 	$('#educacion').on('click', function(){
-		$('html, body').animate({scrollTop:800}, 1000);
+		$('html, body').animate({scrollTop:550}, 1000);
 	})
 
-	$('#experiencia').on('click', function(){
-		$('html, body').animate({scrollTop:600}, 1000);
+	$('#habilidades').on('click', function(){
+		$('html, body').animate({scrollTop:1000}, 1000);
 	})
 
 	$('#portafolio').on('click', function(){
-		$('html, body').animate({scrollTop:700}, 1000);
+		$('html, body').animate({scrollTop:0}, 1000);
 	})
 
 }
