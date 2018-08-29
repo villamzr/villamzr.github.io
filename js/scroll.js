8d074a6e59f2f375441c2a539a7d7c6ceefae306
villamzr <villamzar@gmail.com>
2018-08-28 19:25:32 -0500

    agrerué mejoras en los estilos, escribí los párrafos SOBRE MI y EDUCACION, ajusté el scroll para la version móvil, agregué los logos de la UNICOR y la UNIALICANTE, agregué la barra separadoras vertical y las horizontales, agregué los pie de logo en forma de PÁRRAFOS, y los ajusté con CSS

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,54 @@
-$('#profile').on('click', function(){
-	$('html, body').animate({scrollTop:0}, 1000);
-})
 
-$('#sobremi').on('click', function(){
-	$('html, body').animate({scrollTop:500}, 1000);
-})
+if (screen.width <=700) {
 
-$('#educacion').on('click', function(){
-	$('html, body').animate({scrollTop:1000}, 1000);
-})
+	$('#prof').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
 
-$('#experiencia').on('click', function(){
-	$('html, body').animate({scrollTop:600}, 1000);
-})
+	$('#profile').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
 
-$('#portafolio').on('click', function(){
-	$('html, body').animate({scrollTop:700}, 1000);
-})
+	$('#sobremi').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
+
+	$('#educacion').on('click', function(){
+		$('html, body').animate({scrollTop:800}, 1000);
+	})
+
+	$('#experiencia').on('click', function(){
+		$('html, body').animate({scrollTop:600}, 1000);
+	})
+
+	$('#portafolio').on('click', function(){
+		$('html, body').animate({scrollTop:700}, 1000);
+	})
+
+}else{
+
+	$('#prof').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
+
+	$('#profile').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
+
+	$('#sobremi').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
+
+	$('#educacion').on('click', function(){
+		$('html, body').animate({scrollTop:800}, 1000);
+	})
+
+	$('#experiencia').on('click', function(){
+		$('html, body').animate({scrollTop:600}, 1000);
+	})
+
+	$('#portafolio').on('click', function(){
+		$('html, body').animate({scrollTop:700}, 1000);
+	})
+
+}
