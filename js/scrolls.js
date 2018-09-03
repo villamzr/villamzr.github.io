425dbb8eed9a2b1a9284043ef60467473c50e8ac
villamzr <villamzar@gmail.com>
2018-09-03 18:33:24 -0500

    1. configuré el TERCER SALTO responsivo 2. reorganicé el menú, ahora es horizontal y arriba. 3. aumenté el padding horizontal para el CONTENEDOR DE SECCIONES 4. aumenté el tamaño de imagen de la portada 5. aumenté el tamño de la descripción (nombre, carrera, redes sociales)

--- js/scrolls.js
+++ js/scrolls.js
@@ -1,29 +1,26 @@
 $(document).ready(function(){
 
-		if ($(window).width() <= 639) {
+	$('#perfilCabecera').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	})
 
-		$('#perfilCabecera').on('click', function(){
-			$('html, body').animate({scrollTop:0}, 1000);
-		})
+	$('#profile').on('click', function(){
+		$('html, body').animate({scrollTop:0}, 1000);
+	});
 
-		$('#profile').on('click', function(){
-			$('html, body').animate({scrollTop:0}, 1000);
-		});
+	$('#sobremi').on('click', function(){
+		$('html, body').animate({scrollTop:$('.sobremi').offset().top}, 1000);
+	});
 
-		$('#sobremi').on('click', function(){
-			$('html, body').animate({scrollTop:$('.sobremi').offset().top}, 1000);
-		});
+	$('#educacion').on('click', function(){
+		$('html, body').animate({scrollTop:$('.educacion').offset().top}, 1000);
+	});
 
-		$('#educacion').on('click', function(){
-			$('html, body').animate({scrollTop:$('.educacion').offset().top}, 1000);
-		});
+	$('#habilidades').on('click', function(){
+		$('html, body').animate({scrollTop:$('.habilidades').offset().top}, 1000);
+	});
 
-		$('#habilidades').on('click', function(){
-			$('html, body').animate({scrollTop:$('.habilidades').offset().top}, 1000);
-		});
-
-		$('#portafolio').on('click', function(){
-			$('html, body').animate({scrollTop:$('.portafolio').offset().top}, 1000);
-		});
-	}
+	$('#portafolio').on('click', function(){
+		$('html, body').animate({scrollTop:$('.portafolio').offset().top}, 1000);
+	});
 })
